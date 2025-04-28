3ae9aa9d95e9ef9876822a00622a7e1704d8fa12
ItzYanick <me@yanick.gay>
2025-04-28 09:32:54 +0200

    add displaylink

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
+
+dnf5 install -y /ctx/fedora-42-displaylink-1.14.9-2.github_evdi.x86_64.rpm
 
 # Use a COPR Example:
 #
